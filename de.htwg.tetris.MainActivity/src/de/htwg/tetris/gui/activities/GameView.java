a7e9195ded3835eb64a865af45b2d7f2bbe22150
unknown <ursen@basketball.de>
2012-07-20 02:57:22 +0200

    added gamefield

--- de.htwg.tetris.MainActivity/src/de/htwg/tetris/gui/activities/GameView.java
+++ de.htwg.tetris.MainActivity/src/de/htwg/tetris/gui/activities/GameView.java
@@ -1,5 +1,10 @@
 package de.htwg.tetris.gui.activities;
 
+import de.htwg.tetris.controller.GameController;
+import de.htwg.tetris.model.IGameArray;
+import de.htwg.tetris.model.IQuader.states;
+import de.htwg.tetris.model.ITetrisColor;
+import de.htwg.tetris.observer.IObserver;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -8,9 +13,22 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class GameView extends View {
+public class GameView extends View implements IObserver {
 	
-	Paint tetrisPaint;
+	private Paint tetrisPaint;
+	private IGameArray gameArray = null;
+	private Canvas myCanvas;
+	private int height;
+	private int width;
+	private int windowHeight;
+	private int windowWidth;
+	private int left;
+	private int top;
+	private int right;
+	private int bottom;
+	private int blockSize;
+	private int heightNumber = 20;
+	private int widthNumber = 10;
 
 	public GameView(Context context) {
 		super(context);
@@ -30,24 +48,89 @@ public class GameView extends View {
 	private void init() {
 		setBackgroundColor(Color.BLACK);
 		tetrisPaint = new Paint();
+		this.gameArray = GameController.INSTANCE.getSpielarray();
+		this.gameArray.registerObserver(this);
 	}
 	
+	@Override
+	 public void onWindowFocusChanged(boolean hasFocus) {
+	    super.onWindowFocusChanged(hasFocus);
+		height = this.getHeight();
+		width = this.getWidth();
+		calcGameWindowSize(height,width);
+	 }
+
 	public void onDraw(Canvas canvas){
 		
-		int left = 10;
-		int top = 10;
-		int right = 20;
-		int bottom = 20;
+		myCanvas = canvas;
 		
 		//game area
 		tetrisPaint.setColor(Color.GRAY);
 		tetrisPaint.setStyle(Paint.Style.FILL);
 		canvas.drawRect(left, top, right, bottom, tetrisPaint);
 		
-		//rahmen
+		//frame
 		tetrisPaint.setColor(Color.RED);
 		tetrisPaint.setStyle(Paint.Style.STROKE);
 		canvas.drawRect(left, top, right, bottom, tetrisPaint);
 	}
+	
+	private void calcGameWindowSize(int viewHeight, int viewWidth) {
+		
+		if((viewHeight == 0) || (viewWidth == 0)) {
+			//erreur
+		}
+		if(viewHeight > 2*viewWidth) {
+			//more height than usable -> width is limit
+			windowWidth = calcNextTenWidth(viewWidth);
+			windowHeight = 2*windowWidth;
+			left = (viewWidth-windowWidth)/2;
+			right = left+windowWidth;
+			top = (viewHeight-windowHeight)/2;
+			bottom = top+windowHeight;
+		} else if(viewHeight < viewWidth*2) {
+			//more width than usable -> height is limit
+			windowHeight = calcNextTwentyHeight(viewHeight);
+			windowWidth = windowHeight/2;
+			left = (viewWidth-windowWidth)/2;
+			right = left+windowWidth;
+			top = (viewHeight-windowHeight)/2;
+			bottom = top+windowHeight;
+		}
+		
+	}
+	
+	//calculates the next smaller number divisible by ten
+	private int calcNextTenWidth(int num) {
+		//num = num-2;
+		num = num/widthNumber;
+		blockSize = num;
+		num = num*widthNumber;
+		return num;
+	}
+	
+	private int calcNextTwentyHeight(int num) {
+		//num = num-2;
+		num = num/heightNumber;
+		blockSize = num;
+		num = num*heightNumber;
+		return num;
+	}
 
+	@Override
+	public void update() {
+		repaint(myCanvas);
+	}
+
+	private void repaint(Canvas canvas) {
+		for (int i = 0; i < widthNumber; i++) {
+			for (int j = 0; j < heightNumber; j++) {
+				if (gameArray.getState(i, j) != states.FREE) {
+					ITetrisColor c = gameArray.getColor(i,j);
+					tetrisPaint.setARGB(c.getA(), c.getR(), c.getG(), c.getB());
+					canvas.drawRect(i*left,j*top,left+i*blockSize,top+j*blockSize,tetrisPaint);
+				}
+			}
+		}
+	}
 }
